536ae7b61ee632c6f87003482616f8a4900efbee
Ruizennis <jcennisruiz@protonmail.com>
2026-07-09 22:07:52 -0500

    Update TermuxC.py
    added configuration

--- TermuxC.py
+++ TermuxC.py
@@ -3,13 +3,15 @@ import base64
 import sys
 from time import sleep
 from threading import Lock
+# configuration
+Sleeptime = 0.5
 
 lock = Lock()
 
 def Copy(string):
     with lock:
         content = str(string)
-        b64 = base64.b64encode(content.encode()).decode()
+        b64 = base64.b64encode(content.encode('utf-8')).decode('ascii')
         sys.stdout.write(f"\033]52;c;{b64}\a")
         sys.stdout.flush()
-        sleep(0.5)
+        sleep(Sleeptime)
